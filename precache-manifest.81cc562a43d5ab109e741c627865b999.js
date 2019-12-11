1257b0bcc2592e4a5b19ff0dc39dbd138678213d
Borys Ślaski <borys.slaski@gmail.com>
2019-12-11 10:26:03 +0100

    Updates

---
+++ precache-manifest.81cc562a43d5ab109e741c627865b999.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42df372ed121a95521a4fbc3ca500491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daf7e7ea9c534fcb0e67",
+    "url": "/static/css/main.6342227e.chunk.css"
+  },
+  {
+    "revision": "a4dfd9b4f90c3ca12e6d",
+    "url": "/static/js/2.f358df16.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f358df16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daf7e7ea9c534fcb0e67",
+    "url": "/static/js/main.f20cc343.chunk.js"
+  },
+  {
+    "revision": "b4a81c4240c146151ec1",
+    "url": "/static/js/runtime-main.06faf53c.js"
+  },
+  {
+    "revision": "1d59156926d2c72f6fd119c45132e943",
+    "url": "/static/media/beach-1449008_1280.1d591569.jpg"
+  },
+  {
+    "revision": "add084edb3af073d5c19f68a8221b3bc",
+    "url": "/static/media/logo.add084ed.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project/precache-manifest.6d5cefa1353c61711a9d377c2a20d4a4.js"
+  "/precache-manifest.81cc562a43d5ab109e741c627865b999.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
